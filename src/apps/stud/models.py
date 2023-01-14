44a16d0f7cc9b1ac1014638b40a600266449b228
Alexandr Usenko <usenko.alexandr97@mail.ru>
2023-01-14 20:46:45 +0300

    add unique together

--- src/apps/stud/models.py
+++ src/apps/stud/models.py
@@ -75,14 +75,14 @@ class Class(models.Model):
     name = models.CharField(max_length=1, choices=NAME_CHOICES)
     subjects = models.ManyToManyField('Subject')
     year = models.DateField(auto_now=False, auto_now_add=False)
-    # unique_together = (grade, name, year)
-     
-    def __str__(self):
-        return str(self.grade) + '-' + str(self.name)
 
     class Meta:
+        unique_together = ('grade', 'name', 'year')
         verbose_name = 'Class'
         verbose_name_plural = 'Classes'
+
+    def __str__(self):
+        return str(self.grade) + '-' + str(self.name)
 
 class Subject(models.Model):
     name = models.CharField(max_length=20)
