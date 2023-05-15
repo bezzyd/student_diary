a880659f065e839184acf7136143e7befbfcf057
Alexandr Usenko <usenko.alexandr97@mail.ru>
2023-05-15 16:53:44 +0300

    add student view

--- src/apps/users/api/permissions.py
+++ src/apps/users/api/permissions.py
@@ -7,3 +7,11 @@ class IsAdminOrReadOnly(BasePermission):
             return True
 
         return bool(request.user and request.user.is_staff)
+
+
+class IsTeacher(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return bool(request.studentgroup.classroom_teacher)
--- src/apps/users/api/views_students_group.py
+++ src/apps/users/api/views_students_group.py
@@ -1,8 +1,11 @@
 from rest_framework import mixins
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import GenericViewSet, generics
 
 from src.apps.base.api.mixins import SerializerPerActionMixin
 from src.apps.users.models.student_groups import StudentGroup
+from src.apps.users.models.profiles import StudentProfile
+from src.apps.users.api.permissions import IsTeacher
+from src.apps.users.api.serializers import UserSerializer
 
 
 class StudentGroupViewSet(
@@ -16,3 +19,15 @@ class StudentGroupViewSet(
         "default": StudentGroupRetrieveCreateSerializer,
         "list": StudentGroupListSerializer,
     }
+
+
+class AddStudentInGroup(generics.RetrieveUpdateAPIView):
+    queryset = StudentProfile.objects.all()
+    serializer_class = UserSerializer  # Допустим, в дальнейшем это будет StudentSerializer
+    permission_classes = (IsTeacher,)
+    # lookup_field = "student__user__username"
+    # lookup_url_kwarg = "username"
+
+    def put(self, request, *args, **kwargs):
+        # Нужно дописать условие, что изменятся может только student_profile.student_group
+        return self.update(request, *args, **kwargs)
